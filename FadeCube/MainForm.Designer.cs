17d3f171f4bd154e2a8594b67da844727e499444
libesz <huszty.gergo@digitaltrip.hu>
2011-02-03 15:48:55 +0100

    initial commit in .NET based FadeCube GUI -load and save is working -frames can be added to and removed from the list

---
+++ FadeCube/MainForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace FadeCube
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.frameList = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.newFrameName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.animationOpenDialog = new System.Windows.Forms.OpenFileDialog();
+            this.animationSaveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // frameList
+            // 
+            this.frameList.FormattingEnabled = true;
+            this.frameList.Location = new System.Drawing.Point(12, 12);
+            this.frameList.Name = "frameList";
+            this.frameList.Size = new System.Drawing.Size(120, 199);
+            this.frameList.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(138, 217);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 20);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(138, 12);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 20);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // newFrameName
+            // 
+            this.newFrameName.Location = new System.Drawing.Point(12, 217);
+            this.newFrameName.Name = "newFrameName";
+            this.newFrameName.Size = new System.Drawing.Size(120, 20);
+            this.newFrameName.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(257, 38);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 20);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(257, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 20);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // animationSaveDialog
+            // 
+            this.animationSaveDialog.DefaultExt = "xml";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 262);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.newFrameName);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.frameList);
+            this.Name = "MainForm";
+            this.Text = "FadeCube GUI";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox frameList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox newFrameName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.OpenFileDialog animationOpenDialog;
+        private System.Windows.Forms.SaveFileDialog animationSaveDialog;
+    }
+}
+
---
+++ FadeCube/MainForm.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace FadeCube
+{
+    public partial class MainForm : Form
+    {
+        public CubeAnimationData Animation;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Animation = new CubeAnimationData();
+            frameList.DataSource = Animation.Frames;
+            frameList.DisplayMember = "FrameName";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (newFrameName.Text != "")
+            {
+                CubeAnimation.addFrameToAnimation( Animation, newFrameName.Text );
+                frameList.DataSource = Animation.Frames;
+                frameList.DisplayMember = "FrameName";
+                frameList.SelectedIndex = frameList.Items.Count - 1;
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            //Animation.Frames[frameList.SelectedIndex].FrameData = "bla";
+            if (frameList.SelectedIndex > -1)
+            {
+                CubeAnimation.removeFrameFromAnimation(Animation, frameList.SelectedIndex);
+                frameList.DataSource = Animation.Frames;
+                frameList.DisplayMember = "FrameName";
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            animationOpenDialog.ShowDialog();
+            Animation = CubeAnimation.loadAnimation(animationOpenDialog.FileName);
+            frameList.DataSource = Animation.Frames;
+            frameList.DisplayMember = "FrameName";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            animationSaveDialog.ShowDialog();
+            CubeAnimation.saveAnimation(Animation, animationSaveDialog.FileName);
+        }
+    }
+
+    public class CubeAnimation
+    {
+        public static CubeAnimationData loadAnimation(string path)
+        {
+            StreamReader sr = File.OpenText(path);
+            XmlSerializer serializer = new XmlSerializer(typeof(CubeAnimationData));
+            CubeAnimationData animation = (CubeAnimationData)serializer.Deserialize(sr);
+            sr.Close();
+            return animation;
+        }
+
+        public static void saveAnimation(CubeAnimationData animation, string path)
+        {
+            StreamWriter sw = File.CreateText(path);
+            XmlSerializer serializer = new XmlSerializer(typeof(CubeAnimationData));
+            serializer.Serialize(sw, animation);
+            sw.Close();
+        }
+
+        public static void addFrameToAnimation(CubeAnimationData animation, string frameName)
+        {
+            int i = 0;
+            CubeAnimationFrame[] newFrames = new CubeAnimationFrame[animation.Frames.Length + 1];
+            for (i = 0; i < animation.Frames.Length; i++)
+            {
+                newFrames[i] = animation.Frames[i];
+            }
+            newFrames[i] = new CubeAnimationFrame(frameName, 50);
+            animation.Frames = newFrames;
+        }
+
+        public static void removeFrameFromAnimation(CubeAnimationData animation, int frameNumber)
+        {
+            int i = 0;
+            CubeAnimationFrame[] newFrames = new CubeAnimationFrame[animation.Frames.Length - 1];
+            for (i = 0; i < animation.Frames.Length; i++)
+            {
+                if (i < frameNumber)
+                {
+                    newFrames[i] = animation.Frames[i];
+                }
+                else if (i == frameNumber)
+                {
+                    continue;
+                }
+                else
+                {
+                    newFrames[i - 1] = animation.Frames[i];
+                }
+            }
+            animation.Frames = newFrames;
+        }
+    }
+
+    public class CubeAnimationData
+    {
+        private CubeAnimationGlobalOptions globalOptions;
+        private CubeAnimationFrame[] frames;
+
+        public CubeAnimationData()
+        {
+            globalOptions = new CubeAnimationGlobalOptions( "untitled", 50);
+            frames = new CubeAnimationFrame[1];
+            frames[0] = new CubeAnimationFrame("untitled frame", 50);
+        }
+
+        [XmlElement(ElementName = "GlobalOptions")]
+        public CubeAnimationGlobalOptions GlobalOptions
+        {
+            get
+            {
+                return globalOptions;
+            }
+            set
+            {
+                globalOptions = value;
+            }
+        }
+        [XmlArray(ElementName = "Frames")]
+        public CubeAnimationFrame[] Frames
+        {
+            get
+            {
+                return frames;
+            }
+            set
+            {
+                frames = value;
+            }
+        }
+    } 
+
+    public class CubeAnimationGlobalOptions
+    {
+        private string animationName;
+        private int frameTime;
+
+        public CubeAnimationGlobalOptions( string animationName, int frameTime )
+        {
+            this.animationName = animationName;
+            this.frameTime = frameTime;
+        }
+        public CubeAnimationGlobalOptions() { }
+
+        public string AnimationName
+        {
+            get
+            {
+                return animationName;
+            }
+            set
+            {
+                animationName = value;
+            }
+        }
+        public int FrameTime
+        {
+            get
+            {
+                return frameTime;
+            }
+            set
+            {
+                frameTime = value;
+            }
+        }
+    }
+
+    public class CubeAnimationFrame
+    {
+        private string frameName;
+        private int frameTime;
+        private string frameData;
+
+        public CubeAnimationFrame(string frameName, int frameTime)
+        {
+            this.frameName = frameName;
+            this.frameTime = frameTime;
+        }
+        public CubeAnimationFrame() { }
+
+        public string FrameName
+        {
+            get
+            {
+                return frameName;
+            }
+            set
+            {
+                frameName = value;
+            }
+        }
+        public int FrameTime
+        {
+            get
+            {
+                return frameTime;
+            }
+            set
+            {
+                frameTime = value;
+            }
+        }
+        public string FrameData
+        {
+            get
+            {
+                return frameData;
+            }
+            set
+            {
+                frameData = value;
+            }
+        }
+    }
+}
+
